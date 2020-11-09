d83457687294b74b36158435105a90eb30699cf7
MCSpiderFe <felix-wagenknecht@web.de>
2020-11-09 17:21:13 +0100

    Logging Library includet [spdlog].

--- SpydrEngine/src/Spydr/EntryPoint.h
+++ SpydrEngine/src/Spydr/EntryPoint.h
@@ -6,6 +6,10 @@ extern Spydr::Application* Spydr::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Spydr::Log::Init();
+	SP_CORE_INFO("Initialized Log!");
+	SP_INFO("Initialized Log!");
+
 	auto app = Spydr::CreateApplication();
 	app->Run();
 	delete app;
---
+++ SpydrEngine/src/Spydr/Log.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory.h>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Spydr
+{
+	class SPYDR_API Log
+	{
+	public:
+		static void Init();
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() {return s_CoreLogger;}
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+//Core log macros
+#define SP_CORE_FATAL(...) ::Spydr::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define SP_CORE_ERROR(...) ::Spydr::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define SP_CORE_WARN(...)  ::Spydr::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define SP_CORE_INFO(...)  ::Spydr::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define SP_CORE_TRACE(...) ::Spydr::Log::GetCoreLogger()->trace(__VA_ARGS__)
+
+//Client log macros
+#define SP_FATAL(...) ::Spydr::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define SP_ERROR(...) ::Spydr::Log::GetClientLogger()->error(__VA_ARGS__)
+#define SP_WARN(...)  ::Spydr::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define SP_INFO(...)  ::Spydr::Log::GetClientLogger()->info(__VA_ARGS__)
+#define SP_TRACE(...) ::Spydr::Log::GetClientLogger()->trace(__VA_ARGS__)
