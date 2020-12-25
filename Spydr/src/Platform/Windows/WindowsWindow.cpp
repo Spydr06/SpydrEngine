2d7ca247c22f3f7d337c93a2e952d18def6d0522
MCSpiderFe <felix-wagenknecht@web.de>
2020-12-25 23:25:14 +0100

    Added GLFW callbacks

--- Spydr/src/Platform/Windows/WindowsWindow.cpp
+++ Spydr/src/Platform/Windows/WindowsWindow.cpp
@@ -1,9 +1,18 @@
 #include "sppch.h"
 #include "WindowsWindow.h"
 
+#include "Spydr/Events/KeyEvent.h"
+#include "Spydr/Events/MouseEvent.h"
+#include "Spydr/Events/ApplicationEvent.h"
+
 namespace Spydr
 {
 	static bool s_GLFWInitialized = false;
+
+	static void GLFWErrorCallback(int error, const char* description)
+	{
+		SP_CORE_ERROR("GLFW Error [{0}]: {1}", error, description);
+	}
 
 	Window* Window::Create(const WindowProps& props)
 	{
@@ -26,12 +35,14 @@ namespace Spydr
 		m_Data.Width = props.Width;
 		m_Data.Height = props.Height;
 
-		SP_CORE_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
+		SP_CORE_INFO("Creating window \"{0}\" [{1}, {2}]", props.Title, props.Width, props.Height);
 
 		if (!s_GLFWInitialized) {
 			//TODO: glfwTerminate on system shutdown
 			int success = glfwInit();
 			SP_CORE_ASSERT(success, "Could not initialize GLFW!");
+
+			glfwSetErrorCallback(GLFWErrorCallback);
 
 			s_GLFWInitialized = true;
 		}
@@ -40,6 +51,84 @@ namespace Spydr
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
+
+		// Set GLFW callbacks
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			WindowResizeEvent event(width, height);
+
+			data.Width = width;
+			data.Height = height;
+			data.EventCallback(event);
+		});
+
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			WindowCloseEvent event;
+			data.EventCallback(event);
+		});
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			
+			switch (action) 
+			{
+				case GLFW_PRESS:
+				{
+					KeyPressedEvent event(key, 0);
+					data.EventCallback(event);
+					break;
+				}
+
+				case GLFW_RELEASE:
+				{
+					KeyReleasedEvent event(key);
+					data.EventCallback(event);
+					break;
+				}
+
+				case GLFW_REPEAT:
+				{
+					KeyPressedEvent event(key, 1);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action) {
+				case GLFW_PRESS:
+				{
+					MouseButtonPressedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+
+				case GLFW_RELEASE:
+				{
+					MouseButtonReleasedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xOffset, double yOffset) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseScrolledEvent event((float)xOffset, (float)yOffset);
+			data.EventCallback(event);
+		});
+
+		glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double xPos, double yPos) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseMovedEvent event((float)xPos, (float)yPos);
+			data.EventCallback(event);
+		});
 	}
 
 	void WindowsWindow::Shutdown()
--- Spydr/src/Spydr/Application.cpp
+++ Spydr/src/Spydr/Application.cpp
@@ -1,6 +1,7 @@
 #include "sppch.h"
 #include "Application.h"
 
+#include "Spydr/Events/Event.h"
 #include "Spydr/Events/ApplicationEvent.h"
 #include "Spydr/Log.h"
 
@@ -9,14 +10,26 @@
 namespace Spydr
 {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		SP_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -26,5 +39,12 @@ namespace Spydr
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+
+		return true;
 	}
 }
