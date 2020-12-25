0c9482ccb2f7cd83652f976caf2b74e580e5578d
MCSpiderFe <felix-wagenknecht@web.de>
2020-12-25 19:58:58 +0100

    Added event system

--- Spydr/src/Spydr/EntryPoint.h
+++ Spydr/src/Spydr/EntryPoint.h
@@ -7,7 +7,7 @@ extern Spydr::Application* Spydr::CreateApplication();
 int main(int argc, char** argv)
 {
 	Spydr::Log::Init();
-	SP_CORE_INFO("Initialized Log!");
+	SP_CORE_WARN("Initialized Log!");
 	SP_INFO("Initialized Log!");
 
 	auto app = Spydr::CreateApplication();
---
+++ modules/Hazel/vendor/spdlog/logs/HEAD
@@ -0,0 +1,1 @@
+0000000000000000000000000000000000000000 18e3f07f7d333d33c0ef8fbea02299dbe5d49f97 MCSpiderFe <felix-wagenknecht@web.de> 1604931496 +0100	clone: from https://github.com/gabime/spdlog
