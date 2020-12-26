fbbb283b12cc0b7f37d675d5610a16134a99a50a
MCSpiderFe <felix-wagenknecht@web.de>
2020-12-26 15:00:31 +0100

    Added ImGui events

--- Spydr/src/Spydr/Events/KeyEvent.h
+++ Spydr/src/Spydr/Events/KeyEvent.h
@@ -56,4 +56,21 @@ namespace Spydr
 
 		EVENT_CLASS_TYPE(KeyReleased)
 	};
+
+	class SPYDR_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode) : KeyEvent(keycode)
+		{
+		}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	};
 }
--- Spydr/src/Spydr/ImGui/ImGuiLayer.h
+++ Spydr/src/Spydr/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Spydr/Layer.h"
+#include "Spydr/Events/MouseEvent.h"
+#include "Spydr/Events/KeyEvent.h"
+#include "Spydr/Events/ApplicationEvent.h"
 
 namespace Spydr
 {
@@ -15,6 +18,15 @@ namespace Spydr
 		void OnAttach();
 		void OnDetach();
 	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& event);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& event);
+		bool OnMouseMovedEvent(MouseMovedEvent& event);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& event);
+		bool OnKeyPressedEvent(KeyPressedEvent& event);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& event);
+		bool OnKeyTypedEvent(KeyTypedEvent& event);
+		bool OnWindowResizeEvent(WindowResizeEvent& event);
+
 		float m_Time = 0.0f;
 	};
 }
